a1687a358b8b88869ebfe193d79e87ed38511b20
OFHATWA <tofhatwa@gmail.com>
2023-09-08 16:49:23 +0000

    6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Starting function
+ * Return: Always 0 for success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
