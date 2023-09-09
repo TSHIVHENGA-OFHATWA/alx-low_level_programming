7f7cc58e8229e3897e8b315eb8bcadb5c50ebf90
OFHATWA <tofhatwa@gmail.com>
2023-09-09 05:44:06 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,21 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-		for (b = a; b <= 99; b++)
+		for (b = 0; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-				if (a != 99 || b != 99)
+			if (a < b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
 				{
 					putchar(',');
 					putchar(' ');
-				}
+				}	
+			}
 		}
 	}
 	putchar('\n');
