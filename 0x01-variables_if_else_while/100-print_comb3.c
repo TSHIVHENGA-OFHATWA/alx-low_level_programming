2b8b857a8331fbd83a3e367f4cd8edb8137da4ca
OFHATWA <tofhatwa@gmail.com>
2023-09-10 14:57:08 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,8 +7,6 @@ int main(void)
 {
 	int a, b;
 
-	int c = 0;
-
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = a + 1; b <= '9'; b++)
@@ -17,9 +15,9 @@ int main(void)
 			putchar(b);
 			if (a == '8' && b == '9')
 			{
-				c = 1;
+				break;
 			}
-			if (!c)
+			else
 			{
 				putchar(',');
 				putchar(' ');
