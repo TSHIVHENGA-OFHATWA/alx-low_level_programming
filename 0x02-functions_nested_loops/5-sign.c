201ae7b5f938f8020c0f5edde488e826ef9d18c0
OFHATWA <tofhatwa@gmail.com>
2023-09-12 22:06:09 +0000

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_sign - prints the sign of a number 
- * @n: The input as an integer
+ * print_sign - prints the sign of a number
+ * @num: The input as an integer
  * Return: 1 is greater than zero, 0 is zero, 1 is less than zero
  */
 int print_sign(int num)
