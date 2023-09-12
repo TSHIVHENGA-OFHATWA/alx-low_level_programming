f62d21b5b25b740476df7734bdc20aabd1156cb5
OFHATWA <tofhatwa@gmail.com>
2023-09-12 22:02:43 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computer the absolute value of an integer
+ * @num: input number as an integer
+ * Return: absolute value
+ */
+int _abs(int num);
+{
+	if ( num <= 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
