aff0d445af8263956373070bc803b724073031b5
OFHATWA <tofhatwa@gmail.com>
2023-09-12 22:03:56 +0000

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,14 +4,14 @@
  * @n: The input as an integer
  * Return: 1 is greater than zero, 0 is zero, 1 is less than zero
  */
-int print_sign(int n)
+int print_sign(int num)
 {
-	if (n > 0)
+	if (num > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (num < 0)
 	{
 		_putchar('-');
 		return (-1);
