d0cce93c9ed91abbf42819cae8dd2b2d6a0e9550
OFHATWA <tofhatwa@gmail.com>
2023-09-12 22:19:31 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table
+ * Return: no return
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int mul;
+
+	for (a = 0; a <= 9; a++)
+	{
+
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			mul = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (mul <= 9)
+			{
+				_putchar(32);
+				_putchar(mul + 48);
+			}
+			else
+			{
+				_putchar((mul / 10) + 48);
+				_putchar((mul % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
