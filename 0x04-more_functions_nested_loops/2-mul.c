379b4f59cfc80c8d317a36df61af663dc1a2db24
OFHATWA <tofhatwa@gmail.com>
2023-09-15 02:49:10 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of two the number
+ */
+int multi(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+	return (multi);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	int number = 48;
+	while(number <= 57)
+	{
+		_putchar(number);
+		number++;
+	}
+	_putchar('\n');
+}
