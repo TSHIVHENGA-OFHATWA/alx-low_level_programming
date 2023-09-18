918457379c9a80aac044cfe2ceb043b6f886c450
OFHATWA <tofhatwa@gmail.com>
2023-09-18 22:34:35 +0200

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str: string to be printed.
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	if (str == '\0')
+	{
+		break;
+	}
+	return (str[a]);
+	-putchar('\n');
+}
