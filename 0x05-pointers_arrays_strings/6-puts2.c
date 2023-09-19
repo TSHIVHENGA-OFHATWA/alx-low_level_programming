d199ec96aaca269f28d7fee342ed5e784e8fb264
OFHATWA <tofhatwa@gmail.com>
2023-09-19 02:33:42 +0200

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: argument of string
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a += 2;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - function that prints half of a string, followed by a new line.
+ * @str: argument string
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+
+	int i;
+	int n;
+
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+	}
+	else
+	{
+		n = (length - 1) / 2;
+	}
+	for (i = n; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
