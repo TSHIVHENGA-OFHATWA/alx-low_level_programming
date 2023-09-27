51db3273f6f776bc5c0813ca912943f0b9c20f7b
OFHATWA <tofhatwa@gmail.com>
2023-09-28 00:20:50 +0200

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,12 +12,9 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else if (n > 0)
+	else if (n == 0)
 	{
-		return (n * factorial(n - 1));
+		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (n * factorial(n - 1));
 }
