5ae016081b8de03cda4b305e8b6d4a3b0e501b96
OFHATWA <tofhatwa@gmail.com>
2023-09-27 22:59:07 +0200

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print length of the string.
+ * @s: the string that I need length of.
+ * Return: strong length or 0 for NULL.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
