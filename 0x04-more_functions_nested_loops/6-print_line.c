5f44eba621427f416c2f088254e46414f3a74e70
OFHATWA <tofhatwa@gmail.com>
2023-09-30 20:38:53 +0200

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draw a staright line on the terminal.
+ * @n: parameter passed as integer n.
+ */
+
+void print_line(int n)
+{
+	int i;
+	if (n > 0)
+	{	
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
