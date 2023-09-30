df841245c566d76ced37325261cdd0bcca38315f
OFHATWA <tofhatwa@gmail.com>
2023-09-30 20:59:55 +0200

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,12 @@
  * more_numbers - print 10 times the numbers from 0  to 14
  * followed by a new line
  */
+
 void more_numbers(void)
 {
 	int line, num;
 
-	for (line = 0; line <= 10; line++)
+	for (line = 0; line < 10; line++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
