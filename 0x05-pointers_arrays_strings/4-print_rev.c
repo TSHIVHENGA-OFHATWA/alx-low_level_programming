134a229beb66e72cb4d0d45f95d5f8d0c8841361
OFHATWA <tofhatwa@gmail.com>
2023-10-01 15:50:02 +0200

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * print_rev -  function that prints a string, in reverse.
@@ -10,9 +9,9 @@ void print_rev(char *s)
 {
 	int a, length;
 
-	length = strlen(s);
+	length = _strlen(s);
 
-	for (a = length -1; a >= 0; a--)
+	for (a = length - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 		if (*s == '\0')
@@ -23,3 +22,22 @@ void print_rev(char *s)
 	_putchar('\n');
 }
 
+/**
+ *_strlen - function that returns the length of a string.
+ * @s: The parameter passed as character of string.
+ * Return: string's length.
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
