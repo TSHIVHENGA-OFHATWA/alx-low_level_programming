090a0e0b1dbd216afcb4d29c9ad0610d306443d1
OFHATWA <tofhatwa@gmail.com>
2023-10-11 00:03:54 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,14 @@ void print_dog(struct dog *d)
 	}
 
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else if (d->owner == NULL)
-		printf("owner: (nil)");
+		printf("owner: (nil)\n");
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else if
+		printf("Name: %s\n", d->name);
+	else if
+		printf("Age: %f\n", d->age);
+	else if
+		printf("Owner: %s\n", d->owner);
 }
