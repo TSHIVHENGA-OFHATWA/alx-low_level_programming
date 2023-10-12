029dd53e572e8a833cfef6496b043c2473471cce
OFHATWA <tofhatwa@gmail.com>
2023-10-12 04:44:07 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,13 +10,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || NULL)
+	unsigned long int i;
+
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	while (size-- > 0)
+	for (i = 0; i < size; i++)
 	{
-		action(*array);
-		array++;
+		action(array[i]);
 	}
 }
