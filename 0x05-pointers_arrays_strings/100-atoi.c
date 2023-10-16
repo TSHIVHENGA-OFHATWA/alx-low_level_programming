0af4733899d2e23c44274425ebfe6777e4fa33e6
OFHATWA <tofhatwa@gmail.com>
2023-10-16 21:35:55 +0200

    9-strcpy.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,19 +12,19 @@ int _atoi(char *s)
 
 	while (*s != '\0')
 	{
-	if (*s == '-')
-	{
-		sign *= -1;
-	}
-	else if (*s >= '0' && *s <= '9')
-	{
-		num = num * 10 + (*s - '0');
-	}
-	else if (num > 0)
-	{
-		break;
-	}
-	s++;
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = num * 10 + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		s++;
 	}
 
 	return (num * sign);
