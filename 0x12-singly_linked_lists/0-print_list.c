aa2605c6af432bb5dcd2b459362e7959e153b157
OFHATWA <tofhatwa@gmail.com>
2023-10-20 23:59:39 +0200

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: header pointing to the first node.
+ * Return: the number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i, length = 0;
+	if (h == NULL)
+		return (-1);
+	
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+			length++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			length++;
+		}
+		h = h->next;
+	}
+	return (length);
+}
