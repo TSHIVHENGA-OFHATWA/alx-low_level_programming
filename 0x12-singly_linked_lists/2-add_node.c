006a71c9fd6d78786e32a38a6eb24268e6790e9e
OFHATWA <tofhatwa@gmail.com>
2023-10-22 05:52:14 +0200

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,7 +24,14 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new_node->len = strlen(str);
 	new_node->str = strdup(str);
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = *head;
+
 	*head = new_node;
 
 	return (new_node);
