6090d95a163503767e4ab5c599b8727c3b086af4
OFHATWA <tofhatwa@gmail.com>
2023-10-25 00:59:57 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *current;
+	size_t i = 0;
 
 	if (head == NULL)
 		return (NULL);
@@ -25,11 +26,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 	current = *head;
-	idx--;
-	while (idx != 1)
+	while (i < idx - 1 && current != NULL)
 	{
 		current = current->next;
-		idx--;
+		i++;
 	}
 	if (current == NULL)
 	{
