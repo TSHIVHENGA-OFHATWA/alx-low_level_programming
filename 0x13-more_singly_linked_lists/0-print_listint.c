e6ccbdf0d5bbbd43a3d559f38887ca0e1395a235
OFHATWA <tofhatwa@gmail.com>
2023-10-24 03:00:37 +0200

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @head: head pointing to first node
+ * Return: the number of nodes 
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+/**	if (h == NULL)
+		return (0);*/
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
