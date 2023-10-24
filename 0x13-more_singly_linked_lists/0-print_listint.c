ad5be1eea0c429ce62e99c341bbb8a4bd7630e74
OFHATWA <tofhatwa@gmail.com>
2023-10-24 03:50:50 +0200

    1-listint_len.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	const listint_t *ptr = h;
+	size_t count = 1;
 
 	if (h == NULL)
 		return (0);
 
-	while (h != NULL)
+	while (ptr->next != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", ptr->n);
 		count++;
+		ptr = ptr->next;
 	}
+	printf("%d\n", ptr->n);
 	return (count);
 }
