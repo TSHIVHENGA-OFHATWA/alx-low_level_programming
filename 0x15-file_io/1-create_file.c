fd4e1cd3c2a980b1599de66613f29d34c4a76c3a
OFHATWA <tofhatwa@gmail.com>
2023-10-31 01:19:50 +0200

    0-read_textfile.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: filename to read
+ * @letters: The number of letters to read and print
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	FILE *file;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	file = fopen(filename, "r");
+
+	if (file == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	bytes_read = fread(buffer, 1, letters, file);
+
+	if (bytes_read <= 0)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+	free(buffer);
+	fclose(file);
+	return (bytes_written);
+}
