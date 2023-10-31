20ea77dd1a54644abd710f6e69817fee62b4b35c
OFHATWA <tofhatwa@gmail.com>
2023-10-31 02:14:58 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,53 +1,35 @@
 #include "main.h"
+#include <fcntl.h>
 
 /**
- * read_textfile -  reads a text file and
- * prints it to the POSIX standard output.
- * @filename: filename to read
- * @letters: The number of letters to read and print
- * Return: the actual number of letters it could read and print
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
+ * create_file - Creates a file with specified permissions
+ * and writes content to it
+ * @filename: The name of the file to create or truncate
+ * @text_content: The text to write to the file (NULL for an empty file).
+ * Return: 1 for success, -1 for failure.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content)
 {
-	FILE *file;
-	char *buffer;
-	ssize_t bytes_read, bytes_written;
+	int file_fd;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
-	file = fopen(filename, "r");
+	file_fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_fd == -1)
+		return (-1);
 
-	if (file == NULL)
-		return (0);
-
-	buffer = malloc(letters);
-	if (buffer == NULL)
+	if (text_content != NULL)
 	{
-		fclose(file);
-		return (0);
+		bytes_written = write(file_fd, text_content, strlen(text_content));
+		if (bytes_written != (ssize_t)strlen(text_content))
+		{
+			close(file_fd);
+			return (-1);
+		}
 	}
-
-	bytes_read = fread(buffer, 1, letters, file);
-
-	if (bytes_read <= 0)
-	{
-		free(buffer);
-		fclose(file);
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written != bytes_read)
-	{
-		free(buffer);
-		fclose(file);
-		return (0);
-	}
-	free(buffer);
-	fclose(file);
-	return (bytes_written);
+	close(file_fd);
+	return (1);
 }
