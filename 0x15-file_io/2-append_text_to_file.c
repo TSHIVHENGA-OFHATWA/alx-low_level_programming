0a47743f2ec8514ad07a4ee64884a22345859d18
OFHATWA <tofhatwa@gmail.com>
2023-10-31 03:21:46 +0200

    love

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,8 +15,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
-	else
 		length = strlen(text_content);
 
 	file_fd = open(filename, O_WRONLY | O_APPEND);
@@ -27,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	bytes_written = write(file_fd, text_content, length);
 	close(file_fd);
 
-	if (bytes_written != length)
+	if (bytes_written == -1 || bytes_written == -1)
 		return (-1);
 
 	return (1);
