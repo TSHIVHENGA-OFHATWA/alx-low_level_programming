dbea5ef0eff1998a854d4c81e76f61872a9b6142
OFHATWA <tofhatwa@gmail.com>
2023-10-31 03:16:37 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,20 +9,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_fd;
-	size_t bytes_written, length = 0;
+	int file_fd, bytes_written, length = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+	else
+		length = strlen(text_content);
 
 	file_fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_fd == -1)
-		return (-1)
+		return (-1);
 
-	length = strlen(text_content);
 	bytes_written = write(file_fd, text_content, length);
-	fclose(file);
+	close(file_fd);
 
 	if (bytes_written != length)
 		return (-1);
