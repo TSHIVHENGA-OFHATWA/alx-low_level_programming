843050e8c6af7b83d54cea82ed3826736d6e469e
OFHATWA <tofhatwa@gmail.com>
2024-04-14 04:18:46 +0200

    string compare function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 for same string, 1 for 1st string or -1 for 2nd string.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int loop, equal = 0;
+
+	for (loop = 0; s1[loop] != '\0' || s2[loop] != '\0'; loop++)
+	{
+		if (s1[loop] != s2[loop])
+		{
+			equal = 1;
+			break;
+		}
+	}
+	if (equal == 1)
+	{
+		return (s1[loop] - s2[loop]);
+	}
+	else 
+	{
+		return (s1[loop] - s2[loop]);
+	}
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
