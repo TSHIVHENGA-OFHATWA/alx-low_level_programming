8a7ec7e8392075a0bf6ba4ff28cbe80a7ca107cd
OFHATWA <tofhatwa@gmail.com>
2024-05-04 14:49:32 +0200

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of string.
+ * Return: A capitalized strings.
+ */
+char *cap_string(char *str)
+{
+	int cap_next = 1, i;
+	int length = strlen(str);
+
+	for (i = 0; i < length; i++)
+	{
+		if (is_separator(str[i]))
+		{
+			cap_next = 1;
+		}
+		else if (cap_next)
+		{
+			str[i] = toupper(str[i])
+			cap_next = 0;
+		}
+		else
+		{
+			str[i] = tolower(str[i]);
+		}
+
+	}
+}
+
+int is_separator(char c) 
+{
+	return c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' || c == '.' || c == '!' || c == '?' || c == '"' || c == '(' || c == ')' || c == '{' || c == '}';
+}
