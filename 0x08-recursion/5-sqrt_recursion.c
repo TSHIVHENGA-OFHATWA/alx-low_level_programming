19507869e59e1472a928c40053f565692bb97d8d
OFHATWA <tofhatwa@gmail.com>
2024-05-13 01:00:23 +0200

    pow,sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int helper(int n, int start, int end);
+int helper(int n, int square);
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n:the number to find the square root of
@@ -12,34 +12,22 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
-	return (helper(n, 1, n));
+	return (helper(0, n));
 }
 
 
 /**
  * helper - Finds the square root of a number
  * @n: The number to find the square root of
- * @start: The start of the range to search
- * @end: The end of the range to search
+ * @square: The square number
  *
  * Return: The square root of n if found, otherwise -1
  */
-int helper(int n, int start, int end)
+int helper(int n, int square)
 {
-	int mid, square;
-
-	if (start > end)
-	{
+	if (n * n == square)
+		return (square);
+	else if (n > square / 2)
 		return (-1);
-	}
-	mid = (start + end) / 2;
-	square = mid * mid;
-
-	if (square == n)
-		return (mid);
-	else if (square < n)
-		return (helper(n, mid + 1, end));
-	else
-		return (helper(n, start, mid - 1));
+	return (helper(n + 1, square));
 }
-
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int is_divisible(int n, int divisor);
 /**
  * is_prime_number - checks if a given integer is a prime number
  * @n: the integer to check
@@ -9,15 +10,26 @@
 
 int is_prime_number(int n)
 {
-	int loop;
 
 	if (n <= 1) 
 		return (0);
-	for (loop = 2; loop * loop <= n; loop++)
-	{
-		if (n % loop == 0) 
-			return (0); 
-	}
-	return (1); 
+	else if (n == 2)
+		return (0); 
+	return (!is_divisible(n, 2));
 }
 
+/**
+ * is_divisible - checks if n is divisible by any number from 2 to sqrt(n)
+ * @n: the number to check
+ * @divisor: the current divisor to check
+ *
+ * Return: 1 if n is divisible by any number from 2 to divisor, 0 otherwise
+ */
+int is_divisible(int n, int divisor)
+{
+    if (divisor * divisor > n)
+        return 0;
+    if (n % divisor == 0)
+        return 1;
+    return is_divisible(n, divisor + 1);
+}
